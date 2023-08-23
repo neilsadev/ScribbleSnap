68c7259dbe36efb1477e5c97ce4996bfe3918002
neilsadev <neiloyhassan1@gmail.com>
2023-08-23 18:59:20 +0600

    custom quill

--- lib/views/embed/spreadsheet_embed.dart
+++ lib/views/embed/spreadsheet_embed.dart
@@ -7,6 +7,30 @@ class CustomSpreadsheetBlockEmbed extends CustomBlockEmbed {
 
   static const String spreadsheetType = 'custom_spreadsheet';
 
+  Map<String, Map<String, String>> convertToCustomJson(
+      List<List<String>> data) {
+    Map<String, Map<String, String>> result = {};
+
+    List<String> columnLabels = data[0];
+
+    for (int columnIndex = 0;
+        columnIndex < columnLabels.length;
+        columnIndex++) {
+      String columnLabel =
+          String.fromCharCode(65 + columnIndex); // Convert to A, B, C...
+      Map<String, String> columnValues = {};
+
+      for (int rowIndex = 1; rowIndex < data.length; rowIndex++) {
+        String cellValue = data[rowIndex][columnIndex];
+        columnValues[rowIndex.toString()] = cellValue;
+      }
+
+      result[columnLabel] = columnValues;
+    }
+
+    return result;
+  }
+
   static CustomSpreadsheetBlockEmbed fromDocument(Document document) =>
       CustomSpreadsheetBlockEmbed(jsonEncode(document.toDelta().toJson()));
 
